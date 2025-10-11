36b32da3bfec958f48ba51865b82550b9a48b0ad
Rawda Seid <rawdaseali@gmail.com>
2025-10-11 11:09:28 +0300

    updated weather_advice.py with some changes

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,3 @@ monthly_savings = monthly_income - monthly_expenses
 Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print("Your monthly savings are", monthly_savings)
 print("Projected savings after one year, with interest, is: ", Projected_Savings)
-
-
