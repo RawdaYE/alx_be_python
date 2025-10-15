6a549ebf3c5e6e5d327f4604bd35202570a48000
Rawda Seid <rawdaseali@gmail.com>
2025-10-15 15:57:31 +0300

    added new folder and file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation):
+   match operation:
+     case  "add":
+         return num1 + num2
+     case "subtract":
+         return num1 - num2
+     case "multiply" :
+        return num1 * num2
+     case "divide":
+         if num2 != 0:
+            return num1/num2
+         else:
+            return "you can't divide by zero"
+     case _:
+         return "Invalid operation. please try again!"
+
+         
+          
+
+    
