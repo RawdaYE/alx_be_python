b48040a9a596f3fa9d601301a80d9e66ffdd54cc
Rawda Seid <rawdaseali@gmail.com>
2025-10-24 23:39:16 +0300

    added a file

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,43 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__is_checked_out =  False
+    
+    def checked_out(self):
+        self.__is_checked_out = True
+    
+    def return_book(self):
+        self.__is_checked_out = False
+    
+
+    def is_available(self):
+        return not self.__is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self.__books = []
+    
+    def add_book(self, book):
+        self.__books.append(book)
+    
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title and book.is_available():
+                book.checked_out()
+                break
+    
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                break
+
+    def list_available_books(self):
+        for book in self.__books:
+             if book.is_available():
+                print(f"{book.title} by {book.author}")
+      
+        
+
